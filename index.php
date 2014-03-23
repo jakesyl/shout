a8d09b4f019f00f22e95bd3ebc3395cbf218be37
jakesyl <jake@lexake.com>
2014-03-22 20:26:26 -0400

    testing on heroku, added an echo
    this is going to be awesome

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <?php 
 //let's get to work 
 $ui = "message|1,1;'this is really dumb'";
+echo $ui;
 function split ($ui){
 $type = explode("|", $ui);
 $lat =  explode (",", $type[2]);
@@ -9,15 +10,14 @@ $messag = $long[2];
 $message = ["type"=>$type1,"lat"=>$lat[1], "long" => $lat[1], "message" => $mesag];
 return $message;
 }
-<?php
  
 /*
  * All database connection variables
  */
- 
+/* 
 define('DB_USER', "root"); // db user
 define('DB_PASSWORD', ""); // db password (mention your db password here)
 define('DB_DATABASE', "androidhive"); // database name
 define('DB_SERVER', "localhost"); // db server
-?>
+*/
 ?> 
